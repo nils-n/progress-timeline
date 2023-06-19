389a31ea6345c0dc4c8cf1d66dcca20e9973e882
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-06-19 02:43:40 +0100

    added filter and modal button functions

--- assets/js/index2.js
+++ assets/js/index2.js
@@ -6,7 +6,7 @@ import { generateUniqueID } from "./helpers";
 const filterBtn = document.getElementById("filter-btn");
 const sortBtn = document.getElementById("sort-btn");
 const storyFormContainer = document.getElementById("story-UI-container");
-const contentContainer = document.getElementById("demo-content-container");
+const contentContainer = document.getElementById("content-wrapper");
 const shareStoryBtn = document.getElementById("share-story-button");
 const submitStoryBtn = document.getElementById("submit-story-btn");
 const dontSubmitStoryBtn = document.getElementById("dont-submit-story-btn");
@@ -45,9 +45,9 @@ async function filterContent() {
       contentElement.classList.add("content-box");
 
       const html = ` 
-      <div data-date="${doc.decade}" class="home-timeline-l-container">
+      <div data-date=${doc.decade} class="home-timeline-l-container">
       <div class="home-year-container">
-        <h2 class="home-text04 h2">${doc.decade}</h2>
+        <h2 class="home-text05 h2">${doc.decade}</h2>
       </div>
       <div class="home-line-container">
         <div class="home-line-container1">
@@ -67,108 +67,18 @@ async function filterContent() {
         </div>
       </div>
       <div class="home-event-text-container">
-        <div class="home-container10">
-          <span class="home-text05">${doc.title}</span>
-          <span class="home-text06">
+        <div class="home-container12">
+          <span class="home-text06">${doc.title}</span>
+          <span class="home-text07">
           ${doc.content}
           </span>
         </div>
       </div>
-      <div class="home-user-modal-container">
-        <h2 class="home-text07">Your storIES</h2>
-        <div class="home-testimonial-card">
-          <div class="home-testimonial">
-            <svg viewBox="0 0 1024 1024" class="home-icon08">
-              <path
-                d="M225 448c123.712 0 224 100.29 224 224 0 123.712-100.288 224-224 224s-224-100.288-224-224l-1-32c0-247.424 200.576-448 448-448v128c-85.474 0-165.834 33.286-226.274 93.726-11.634 11.636-22.252 24.016-31.83 37.020 11.438-1.8 23.16-2.746 35.104-2.746zM801 448c123.71 0 224 100.29 224 224 0 123.712-100.29 224-224 224s-224-100.288-224-224l-1-32c0-247.424 200.576-448 448-448v128c-85.474 0-165.834 33.286-226.274 93.726-11.636 11.636-22.254 24.016-31.832 37.020 11.44-1.8 23.16-2.746 35.106-2.746z"
-              ></path>
-            </svg>
-            <span class="home-text08">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              In lorem lorem, malesuada in metus vitae, scelerisque
-              accumsan ipsum.
-            </span>
-            <div class="home-like-container">
-              <div class="home-container11">
-                <span class="home-text09">
-                  <span class="home-text10">15</span>
-                  <span></span>
-                  <br />
-                </span>
-                <span class="home-text13">Likes</span>
-              </div>
-              <div class="home-container12">
-                <button
-                  id="filter-stories"
-                  type="button"
-                  class="home-button3 button"
-                >
-                  Like
-                </button>
-              </div>
-            </div>
-          </div>
-          <div class="home-container13">
-            <div class="home-container14">
-              <img
-                alt="profile"
-                src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDE0fHxwb3J0cmFpdHxlbnwwfHx8fDE2MjYzNzg5NzI&amp;ixlib=rb-1.2.1&amp;w=200"
-                class="home-image3"
-              />
-              <span class="home-text14">Test User</span>
-            </div>
-          </div>
-        </div>
-        <div class="home-testimonial-card1">
-          <div class="home-testimonial1">
-            <svg viewBox="0 0 1024 1024" class="home-icon10">
-              <path
-                d="M225 448c123.712 0 224 100.29 224 224 0 123.712-100.288 224-224 224s-224-100.288-224-224l-1-32c0-247.424 200.576-448 448-448v128c-85.474 0-165.834 33.286-226.274 93.726-11.634 11.636-22.252 24.016-31.83 37.020 11.438-1.8 23.16-2.746 35.104-2.746zM801 448c123.71 0 224 100.29 224 224 0 123.712-100.29 224-224 224s-224-100.288-224-224l-1-32c0-247.424 200.576-448 448-448v128c-85.474 0-165.834 33.286-226.274 93.726-11.636 11.636-22.254 24.016-31.832 37.020 11.44-1.8 23.16-2.746 35.106-2.746z"
-              ></path>
-            </svg>
-            <span class="home-text15">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              In lorem lorem, malesuada in metus vitae, scelerisque
-              accumsan ipsum.
-            </span>
-            <div class="home-like-container1">
-              <div class="home-container15">
-                <span class="home-text16">
-                  <span class="home-text17">15</span>
-                  <span></span>
-                  <br />
-                </span>
-                <span class="home-text20">Likes</span>
-              </div>
-              <div class="home-container16">
-                <button
-                  id="filter-stories"
-                  type="button"
-                  class="home-button4 button"
-                >
-                  Like
-                </button>
-              </div>
-            </div>
-          </div>
-          <div class="home-container17">
-            <div class="home-container18">
-              <img
-                alt="profile"
-                src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDE0fHxwb3J0cmFpdHxlbnwwfHx8fDE2MjYzNzg5NzI&amp;ixlib=rb-1.2.1&amp;w=200"
-                class="home-image4"
-              />
-              <span class="home-text21">Test User</span>
-            </div>
-          </div>
-        </div>
-      </div>
       <button
-        id="filter-stories-right"
         type="button"
-        class="home-button5 button"
+        class="home-button6 button"
       >
-        Show YOUR STORIES
+        User Stories in ${doc.decade}
       </button>
       </div>
       `;
@@ -177,6 +87,15 @@ async function filterContent() {
 
       contentContainer.appendChild(contentElement);
     });
+
+    // RE-ADD BUTTON FUNCTION TO DYNAMIC BUTTONS
+    const userStoryBtns = Array.from(
+      contentContainer.querySelectorAll(".home-button6")
+    );
+
+    userStoryBtns.forEach((btn) =>
+      btn.addEventListener("click", () => console.log("lol"))
+    );
   } catch (error) {
     console.error("Error retrieving content:", error);
     alert("Failed to retrieve content. Please try again.");
